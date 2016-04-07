2e71185efa7398dbeeff773904202e375e4533e6
Stephen Pearce <stephen.r.pearce@gmail.com>
2016-04-06 20:59:46 -0400

    Added helper methods for rendering views and fetching models, added stub for handling ajax errors, added example fetch/render to router

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,40 @@
 "use strict";
 
+// render method for views
+app.renderView = function(view) {
+  if (app.view) {
+    app.view.remove(); // remove the previous view
+  }
+  app.view = view.render();
+};
+
+// fetches any models or collections listed in the arguments and then renders the view
+app.renderFetch = function(view) {
+  var models = Array.prototype.slice.call(arguments, 1),
+    done = _.partial(app.renderView, view),
+    fetch = [];
+
+  // find any empty models and add them to fetch list
+  _.each(models, function(model) {
+    if (model.isEmpty()) {
+      fetch.push(model.fetch());
+    }
+  });
+
+  // fetch any models/collections if required, then render
+  if (fetch.length > 0) {
+    $.when.apply(this, fetch).then(done, app.ajaxError);
+  }
+  else {
+    done();
+  }
+};
+
+// generic ajax error handler
+app.ajaxError = function(xhr, status, error) {
+  console.error(error); //TODO: render the message in the UI
+};
+
 (function() {
   // global object
   var root = (typeof window == 'object' && window) || global;
--- src/js/routers/router.js
+++ src/js/routers/router.js
@@ -7,8 +7,9 @@ app.Router = Backbone.Router.extend({
 
   // default route
   default: function() {
-    var view = new app.AppView();
-    view.render();
+    var view = new app.AppView({ model: new app.AppModel(), collection: new app.AppModels() });
+    app.renderFetch(view);
+    // models/collections can be fetch as well: app.fetchRender(view, view$.model, view.collection, ...);
   },
 
   navigate: function(fragment, options) {
